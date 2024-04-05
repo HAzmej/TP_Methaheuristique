dc905b366650d3759504485af40af67d604f43b8
HAzmej <mejri.zizou2@yahoo.com>
2024-04-05 11:29:39 +0200

    Fin TP2

--- code-template-main/src/main/java/jobshop/solvers/GreedySolver.java
+++ code-template-main/src/main/java/jobshop/solvers/GreedySolver.java
@@ -1,9 +1,12 @@
 package jobshop.solvers;
 
-import jobshop.Instance;
-import jobshop.encodings.Schedule;
-
+import java.util.ArrayList;
 import java.util.Optional;
+
+import jobshop.Instance;
+import jobshop.encodings.ResourceOrder;
+import jobshop.encodings.Schedule;
+import jobshop.encodings.Task;
 
 /** An empty shell to implement a greedy solver. */
 public class GreedySolver implements Solver {
@@ -23,6 +26,40 @@ public class GreedySolver implements Solver {
 
     @Override
     public Optional<Schedule> solve(Instance instance, long deadline) {
-        throw new UnsupportedOperationException();
+        int nb_tache_per_job = instance.numTasks;
+        int nb_job_instan = instance.numJobs; 
+        ResourceOrder manualRO = new ResourceOrder(instance);
+        ArrayList <Task> tabtask = new ArrayList();
+
+        //Si on a des Task à faire : on crée tous les Jobs
+        if (nb_tache_per_job>0) {
+            for (int i=0; i<nb_job_instan;i++) {
+                tabtask.add(new Task(i,0));
+            }
+        }
+
+        ArrayList <Integer> machine_dispo = new ArrayList<>();
+        for ( int i=0 ;i <instance.numMachines;i++) {
+            machine_dispo.add(i,0);
+        } 
+
+        ArrayList <Integer> job_dispo = new ArrayList<>();
+        for ( int i=0 ;i <instance.numJobs;i++) {
+            job_dispo.add(i,0);
+        } 
+
+        //Traitement des Cas de Priorité
+        while (!tabtask.isEmpty()) {
+            switch(priority) {
+                case SPT: 
+                    System.out.println("Je suis SPT");
+
+                    break;
+                case LRPT : 
+                    System.out.println("Je suis LRPT");
+                    break;
+            }
+        }
+        throw new UnsupportedOperationException();  
     }
 }
